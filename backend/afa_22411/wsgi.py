8fe56e6397206529657e0d5648018c4a431621b8
Crowdbotics <team@crowdbotics.com>
2020-11-08 21:11:38 +0000

    Update models

--- backend/afa_22411/wsgi.py
+++ backend/afa_22411/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'afa_22411.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "afa_22411.settings")
 
 application = get_wsgi_application()
